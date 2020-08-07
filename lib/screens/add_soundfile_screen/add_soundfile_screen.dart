bb072ba2b8e306562dd613e0525a837214f1d7d4
ArneDuyver <arne_duyver@hotmail.com>
2020-08-07 17:53:42 +0200

    add_soundfile_screen

--- lib/screens/add_soundfile_screen/add_soundfile_screen.dart
+++ lib/screens/add_soundfile_screen/add_soundfile_screen.dart
@@ -1,3 +1,8 @@
+import 'dart:io';
+
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:my_own_soundboard/screens/home_screen/home_screen.dart';
 import 'package:my_own_soundboard/screens/widgets/media_button_appbar.dart';
@@ -13,6 +18,39 @@ class AddSoundfileScreen extends StatefulWidget {
 }
 
 class _AddSoundfileScreenState extends State<AddSoundfileScreen> {
+  final myController = TextEditingController();
+
+  String filepath;
+
+  void addFilepath() async {
+    String path;
+    File file = await FilePicker.getFile();
+    path = file.path;
+    setState(() {
+      filepath = path;
+    });
+  }
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  static AudioPlayer _audioPlayer = AudioPlayer();
+  bool isPlaying = false;
+
+  void playAudio(String path) async {
+    print(path);
+    int status = await _audioPlayer.play(path, isLocal: true);
+
+    if (status == 1) {
+      setState(() {
+        isPlaying = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +63,96 @@ class _AddSoundfileScreenState extends State<AddSoundfileScreen> {
             Menu(),
           ],
         ),
-        //TODO: make it pretty
-        body: Text("Hellow"));
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              //TODO: make it pretty
+              Container(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Fill in a title below",
+                  style: TextStyle(fontSize: 26.0),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+                  controller: myController,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Click the music note and find your soundfile",
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  addFilepath();
+                },
+                child: Icon(Icons.music_note),
+              ),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "filepath: $filepath",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.red,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(8.0),
+              ),
+              Container(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Click 'Add sound' to add your sound to your library",
+                  style: TextStyle(fontSize: 18.0),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  // #region Dialog popUp TODO
+                  //TODO if title or filepath are null enter a help dialog
+                  return showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        content: Text(
+                          "Add ${myController.text} with filepath $filepath to library?",
+                          textAlign: TextAlign.center,
+                        ),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text("Yes"),
+                            onPressed: () {
+                              return playAudio(filepath);
+                            },
+                          ),
+                          FlatButton(
+                            child: Text("No"),
+                            onPressed: () {
+                              _audioPlayer.stop();
+                              setState(() {
+                                isPlaying = false;
+                              });
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                  // #endregion
+                },
+                child: Text("Add sound"),
+              ),
+            ],
+          ),
+        ));
   }
 }
